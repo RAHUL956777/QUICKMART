5d10e96677b6dcedae7895ef8165cfa4c532c414
Rahul <dasr16583@gmail.com>
2023-04-08 22:57:27 +0530

    working on privacy policy and about us

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,33 @@ import Layout from "../components/Layout/Layout";
 const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <h2 className="h2">About Us</h2>
+      <div className="about-div">
+        <div className="about-left">
+          <img src="images/about.avif" alt="contact-img" />
+        </div>
+        <div className="about-right">
+          <p>
+            Welcome to our e-commerce web application, where shopping is just a
+            few clicks away! Our platform offers a seamless online shopping
+            experience, providing you with a wide range of products and services
+            from the comfort of your own home. Our user-friendly interface makes
+            it easy for you to browse our extensive catalog of products, which
+            includes everything from clothing and accessories to electronics and
+            home goods. With detailed product descriptions, multiple images, and
+            customer reviews, you can make informed purchasing decisions that
+            meet your needs and budget. Our checkout process is quick and
+            secure, ensuring that your personal and financial information is
+            protected every step of the way. We accept a variety of payment
+            methods, including credit cards, debit cards, and PayPal, to make
+            your shopping experience as convenient as possible. And if you ever
+            need assistance, our friendly customer service team is available to
+            help you with any questions or concerns you may have. So why wait?
+            Start shopping now and experience the convenience of our e-commerce
+            web application today!
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,7 +4,32 @@ import Layout from "../components/Layout/Layout";
 const Policy = () => {
   return (
     <Layout>
-      <h1>privacy policy page</h1>
+      <h2 className="h2">Our Privacy Policys</h2>
+      <div className="policy-div">
+        <p className="p">
+          Company B's Privacy Policy Statement: Company B respects the privacy
+          of our users and takes the responsibility of protecting personal
+          information seriously. We collect personal information for the purpose
+          of improving our services, communicating with users, and processing
+          orders. We may share personal information with third-party service
+          providers who assist us with fulfilling orders and delivering
+          products. We take measures to ensure that personal information is
+          safeguarded against unauthorized access, use, disclosure, and
+          destruction.
+        </p>
+        <p className="p">
+          Company C's Privacy Policy Statement: At Company C, we understand the
+          importance of protecting personal information and are committed to
+          maintaining the privacy of our users. We collect personal information
+          to fulfill orders, provide customer service, and improve our services.
+          We may share personal information with third parties who provide
+          services on our behalf, but only for the purpose of delivering
+          products and improving our services. We use reasonable security
+          measures to protect personal information from unauthorized access,
+          disclosure, alteration, and destruction. We do not sell personal
+          information to third parties.
+        </p>
+      </div>
     </Layout>
   );
 };
