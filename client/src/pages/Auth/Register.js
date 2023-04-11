83b9d4165fb1061fac45adfbb8d7aa1ade9c19e2
Rahul <dasr16583@gmail.com>
2023-04-11 08:49:31 +0530

    sucessfullly fecthing frontend data to backend ushing axios

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useState } from "react";
+import Layout from "./../../components/Layout/Layout";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
 
-export default Register
+  const navigate = useNavigate();
+  // from function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("api/v1/auth/register", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went rong");
+    }
+  };
+  return (
+    <Layout title={"Register - QuickMart"}>
+      <div className="register">
+        <h1 className="h1">Welcome to Register page</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="number"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your phone no"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+              autoComplete="off"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,26 +7,26 @@ export const registerController = async (req, res) => {
     const { name, email, password, phone, address } = req.body;
     // validation
     if (!name) {
-      return res.send({ error: "Name is required" });
+      return res.send({ message: "Name is required" });
     }
     if (!email) {
-      return res.send({ error: "Email is required" });
+      return res.send({ message: "Email is required" });
     }
     if (!password) {
-      return res.send({ error: "Password is required" });
+      return res.send({ message: "Password is required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone no is required" });
+      return res.send({ message: "Phone no is required" });
     }
     if (!address) {
-      return res.send({ error: "Address is required" });
+      return res.send({ message: "Address is required" });
     }
     // check user
     const existingUser = await userModel.findOne({ email });
     // existing user
     if (existingUser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Registered Please Login",
       });
     }
@@ -110,5 +110,5 @@ export const loginController = async (req, res) => {
 
 // test controller
 export const testController = (req, res) => {
- res.send("Protected ROute");
+  res.send("Protected ROute");
 };
