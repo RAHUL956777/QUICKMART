76be746c63edb93f299ba0724b6fe9437061530d
Rahul <dasr16583@gmail.com>
2023-04-17 19:53:09 +0530

    adding context api

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import { useAuth } from "../context/auth";
 
 const HomePage = () => {
+  const { auth, setAuth } = useAuth();
   return (
-    <Layout>
+    <Layout title={"Best-Offers"}>
       <h1>Home page</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </Layout>
   );
 };
