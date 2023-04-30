80d20bb5f15cbc7e174ffdcc8fa3851440641034
Rahul <dasr16583@gmail.com>
2023-04-30 11:36:15 +0530

    Acheaving the loadmore functionality

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -9,7 +9,9 @@ const HomePage = () => {
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
   const [radio, setRadio] = useState([]);
-
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   // get all category
   const getAllCategory = async (req, res) => {
@@ -27,12 +29,15 @@ const HomePage = () => {
     getAllCategory();
   }, []);
 
-  // get products
+  // get all products
   const getAllProducts = async () => {
     try {
-      const { data } = await axios.get("/api/v1/product/get-product");
+      setLoading(true);
+      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      setLoading(false);
       setProducts(data?.products);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
@@ -44,6 +49,38 @@ const HomePage = () => {
   useEffect(() => {
     if (checked.length || radio.length) filterProduct();
   }, [checked, radio]);
+
+  // get total count
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/product-count");
+      setTotal(data?.total);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTotal();
+  }, []);
+
+  // loadmore
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/v1/product/product-list/${page}`);
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
 
   // filter by category
   const handleFilter = (value, id) => {
@@ -128,6 +165,19 @@ const HomePage = () => {
                 </div>
               </div>
             ))}
+          </div>
+          <div className="m-2 p-3">
+            {products && products.length < total && (
+              <button
+                className="btn btn-warning"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? "loading...." : "Loadmore"}
+              </button>
+            )}
           </div>
         </div>
       </div>
