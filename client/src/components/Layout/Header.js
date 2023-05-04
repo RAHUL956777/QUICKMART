330464c3f192e710451617dc19a69f9d62661b66
Rahul <dasr16583@gmail.com>
2023-05-05 01:09:44 +0530

    working on category

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -4,9 +4,12 @@ import { GiShoppingBag } from "react-icons/gi";
 import { useAuth } from "../../context/auth";
 import { toast } from "react-hot-toast";
 import SearchInput from "../Form/SearchInput";
+import useCategory from "../../hooks/useCategory";
 
 const Header = () => {
   const { auth, setAuth } = useAuth();
+  const categories = useCategory();
+
   const handleLogout = () => {
     setAuth({
       ...auth,
@@ -40,16 +43,38 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <SearchInput />
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link " aria-current="page">
-                  Home
-                </NavLink>
+              {/* <li className="nav-item"> */}
+              <NavLink to="/" className="nav-link " aria-current="page">
+                Home
+              </NavLink>
+
+              <li className="nav-item dropdown">
+                <Link
+                  to={"/categories"}
+                  className="nav-link dropdown-toggle"
+                  data-bs-toggle="dropdown"
+                >
+                  Categories
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to={`/categories`}>
+                      All Categories
+                    </Link>
+                  </li>
+                  {categories?.map((c) => (
+                    <li>
+                      <Link
+                        className="dropdown-item"
+                        to={`/category/${c.slug}`}
+                      >
+                        {c.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
               </li>
-              <li className="nav-item">
-                <NavLink to="/category" className="nav-link ">
-                  Category
-                </NavLink>
-              </li>
+
               {!auth.user ? (
                 <>
                   <li className="nav-item">
---
+++ client/src/pages/Categories.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import useCategory from "../hooks/useCategory";
+import Layout from "../components/Layout/Layout";
+
+const Categories = () => {
+  const categories = useCategory();
+  return (
+    <Layout title={"All Categories"}>
+      <div className="container">
+        <div className="row">
+          {categories.map((c) => (
+            <div className="col-md-6 mt-5 mb-3 gx-3 gy-3">
+                <Link to={"/"} className="btn btn-primary">{c.name}</Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Categories;
