879d7de1db02f2c0be883cdcc1f9815e2c21b994
Rahul <dasr16583@gmail.com>
2023-05-13 22:21:58 +0530

    changes some styles in categories

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -7,11 +7,24 @@ const Categories = () => {
   const categories = useCategory();
   return (
     <Layout title={"All Categories"}>
-      <div className="container">
-        <div className="row">
+      <div className="container" style={{ marginTop: "100px" }}>
+        <div className="row container">
+          <h2 className="text-center">All Categories</h2>
           {categories.map((c) => (
-            <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
-                <Link to={`/category/${c.slug}`} className="btn btn-primary">{c.name}</Link>
+            <div className="col-md-4 mt-5 mb-3 gx-2 gy-2" key={c._id}>
+              <div className="card">
+                <Link
+                  to={`/category/${c.slug}`}
+                  className="btn cat-btn hov"
+                  style={{
+                    padding: "22px",
+                    fontSize: "20px",
+                    fontWeight: "40px",
+                  }}
+                >
+                  {c.name}
+                </Link>
+              </div>
             </div>
           ))}
         </div>
