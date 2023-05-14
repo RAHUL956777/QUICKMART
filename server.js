a47c8c3e359164a0d30d2e9387416b0cbb61f1e8
RAHUL <dasr16583@gmail.com>
2023-05-14 19:01:31 +0530

    testing

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/v1/product", productRoutes);
 
 // rest api
 app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // port
