f365e0c968242dcad3391fa62dae3879664d3455
RAHUL <dasr16583@gmail.com>
2023-05-15 21:37:55 +0530

    some changes

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 // configure env
 dotenv.config();
@@ -16,17 +14,12 @@ dotenv.config();
 // database config
 connectDB();
 
-// es modile fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -34,8 +27,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommmerce app </h1>");
 });
 
 // port
